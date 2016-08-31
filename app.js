571f90cacc4dd346d656b44e962c9acd454ecacf
Ben <bgk0018@gmail.com>
2016-08-30 21:23:47 -0500

    Initial pass at understanding socket.io

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 8080;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on('connection', function (socket) {
+
+    socket.on('join-game', function (data) {
+
+        console.log('player has joined the game!');
+
+        socket.broadcast.emit('player-joined', {
+            player: 'A player'
+        });
+    });
+
+});
+
+
+server.listen(port, function () {
+
+    console.log('Listening on port ' + port);
+});
